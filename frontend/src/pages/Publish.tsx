3f9aeacc4f3bf37617afd4da06f01e94f413e3ed
pantha704 <prathamjaiswal204@gmail.com>
2025-08-06 12:37:01 +0530

    optimised_build

--- frontend/src/pages/Publish.tsx
+++ frontend/src/pages/Publish.tsx
@@ -1,6 +1,5 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { AppBar } from "@/components/AppBar";
-import { div } from "motion/react-client";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { BACKEND_URL } from "@/config";
